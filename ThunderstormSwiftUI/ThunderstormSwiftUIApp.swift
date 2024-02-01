4f25d88af285ca933df38c1148e40dc916ec19af
Obinna <macmartins081@gmail.com>
2024-02-02 01:52:27 +0400

    Initial Commit

---
+++ ThunderstormSwiftUI/ThunderstormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderstormSwiftUIApp.swift
+//  ThunderstormSwiftUI
+//
+//  Created by Obinna on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThunderstormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
