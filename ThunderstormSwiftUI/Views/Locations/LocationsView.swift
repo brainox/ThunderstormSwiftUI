6ada0fe38cd610f8db5efe0ef88dc1bd5aa6a489
Obinna <macmartins081@gmail.com>
2024-02-02 02:43:54 +0400

    Created locationsView and LocationViewModel

---
+++ ThunderstormSwiftUI/Views/Locations/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  ThunderstormSwiftUI
+//
+//  Created by Obinna on 02/02/2024.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
