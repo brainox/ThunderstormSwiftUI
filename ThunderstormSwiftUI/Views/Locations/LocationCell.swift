1050731c5b7e8b05ddd923ff063701ef4ab06153
Obinna <macmartins081@gmail.com>
2024-02-06 17:30:50 +0400

    Displaying weather data

---
+++ ThunderstormSwiftUI/Views/Locations/LocationCell.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCell.swift
+//  ThunderstormSwiftUI
+//
+//  Created by Obinna on 06/02/2024.
+//
+
+import SwiftUI
+
+struct LocationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationCell()
+}
