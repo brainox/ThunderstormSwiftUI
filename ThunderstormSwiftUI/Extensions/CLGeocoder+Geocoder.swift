726295190b5d43c882cb3f90b393f1a3c1794dea
Obinna <macmartins081@gmail.com>
2024-02-08 19:07:02 +0400

    Improving the testability of the Geocoding Client.

---
+++ ThunderstormSwiftUI/Extensions/CLGeocoder+Geocoder.swift
@@ -0,0 +1,8 @@
+//
+//  CLGeocoder+Geocoder.swift
+//  ThunderstormSwiftUI
+//
+//  Created by Obinna on 08/02/2024.
+//
+
+import Foundation
---
+++ ThunderstormSwiftUI/Geocoding/Geocoder.swift
@@ -0,0 +1,8 @@
+//
+//  Geocoder.swift
+//  ThunderstormSwiftUI
+//
+//  Created by Obinna on 08/02/2024.
+//
+
+import Foundation
