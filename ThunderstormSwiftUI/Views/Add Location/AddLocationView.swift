42acd6d496fb25586ae033515509c7396c1b4d22
Obinna <macmartins081@gmail.com>
2024-02-09 20:00:30 +0400

    refactoring the add location view

--- ThunderstormSwiftUI/Views/Add Location/AddLocationView.swift
+++ ThunderstormSwiftUI/Views/Add Location/AddLocationView.swift
@@ -21,15 +21,25 @@ struct AddLocationView: View {
         VStack {
             TextField(viewModel.textFieldPlaceholder, text: $viewModel.query)
                 .padding()
-            List {
-                ForEach(viewModel.addLocationCellViewModels) {
-                    cellViewModel in
-                    AddLocationCell(viewModel: cellViewModel) {
-                        viewModel.addLocation(with: cellViewModel.id)
-                        showsAddLocationView.wrappedValue.toggle()
+            switch viewModel.state {
+            case .empty:
+                Spacer()
+            case .querying:
+                MessageView(style: .progressView)
+            case .message(let message):
+                MessageView(style: .message(message))
+            case .results(let viewModels):
+                List {
+                    ForEach(viewModels) {
+                        cellViewModel in
+                        AddLocationCell(viewModel: cellViewModel) {
+                            viewModel.addLocation(with: cellViewModel.id)
+                            showsAddLocationView.wrappedValue.toggle()
+                        }
                     }
                 }
             }
+            
         }
     }
 }
@@ -38,4 +48,29 @@ struct AddLocationView: View {
     let viewModel = AddLocationViewModel(geocodingService: GeocodingPreviewClient())
     return AddLocationView(viewModel: viewModel,
                     showsAddLocationView: .constant(true))
+}
+
+fileprivate struct MessageView: View {
+    
+    enum Style {
+        case progressView
+        case message(String)
+    }
+    
+    let style: Style
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            switch style {
+            case .progressView:
+                ProgressView()
+            case .message(let message):
+                Text(message)
+                    .font(.body)
+                    .foregroundStyle(.darkGray)
+            }
+            Spacer()
+        }
+    }
 }
